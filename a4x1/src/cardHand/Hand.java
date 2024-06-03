0b34dd3ac7a0c3a10128a6a4f0b39d8b29e08b91
Kiwi <167688339+yoonniu@users.noreply.github.com>
2024-06-03 22:15:50 +0200

    Update Hand.java

--- a4x1/src/cardHand/Hand.java
+++ a4x1/src/cardHand/Hand.java
@@ -6,97 +6,90 @@ import static cards.Card.*;
 import static cards.Card.Constant.*;
 
 public class Hand {
-    
+
     public Card[] cards;
     public int size;
 
-        // Konstruktor: Nimmt beliebig viele Karten entgegen
-        public Hand(Card... initialCards) { //... damit eine Variable Anzahl an Argumenten des Typs übernehmen werden können
-            this.cards = new Card[initialCards.length];
-            this.size = 0;
-            for (Card card : initialCards) {
-                if (card != null) {
-                    this.cards[size++] = card; //Karten werden in Array gespeichert
-                }
+    // Constructor : accepts any number of cards
+    public Hand(Card... initialCards) {
+        this.cards = new Card[initialCards.length];
+        this.size = 0;
+        for (int i = 0; i < initialCards.length; i++) {
+            Card card = initialCards[i];
+            if (card != null) {
+                this.cards[size++] = card; // load cards in array
             }
-        }
-
-        /*// Prozedur add(): Fügt Karten hinzu
-        public void add(Card... newCards) {
-            for (Card card : newCards) { //Iterieren durch newCards
-                if (card != null) {
-                    if (size == cards.length) { //check if card array is full
-                        expandArray();
-                    }
-                    cards[size++] = card; //iteration, safe card and cards array
-                }
-            }
-        }*/
-        public void add(Card... newCards) {
-            int newSize = size + newCards.length;
-            if (newSize > cards.length) {
-                expandArray(newCards.length);
-            }
-            for (Card card : newCards) {
-                if (card != null) {
-                    cards[size++] = card;
-                }
-            }
-        }
-
-        // Procedure add(): adds cards to another hand
-        public void add(Hand otherHand) {
-            for (Card card : otherHand.getHandCards()) {
-                if (card != null) {
-                    if (size == cards.length) {
-                        expandArray(cards.length);
-                    }
-                    cards[size++] = card;
-                }
-            }
-        }
-
-        // Funktion isSuited(): Überprüft, ob alle Karten dieselbe Farbe haben
-        public boolean isSuited() {
-            if (size == 0) return true; //empty hand > suited
-            Suit suit = cards[0].getSuit(); //color of first card as
-            for (int i = 1; i < size; i++) {
-                if (!cards[i].getSuit().equals(suit)) { 
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        // Funktion getHandCards(): Liefert alle Karten in der Hand
-        public Card[] getHandCards() {
-            Card[] currentCards = new Card[size];
-            System.arraycopy(cards, 0, currentCards, 0, size);
-            return currentCards;
-        }
-
-        // Prozedur setHandCards(): Setzt die Hand auf neue Karten
-        public void setHandCards(Card... newCards) {
-            this.cards = new Card[newCards.length];
-            this.size = 0;
-            for (Card card : newCards) {
-                if (card != null) {
-                    this.cards[size++] = card;
-                }
-            }
-        }
-
-        /*// Hilfsprozedur: Erweitert das Array
-        private void expandArray() {
-            Card[] newCards = new Card[cards.length * 2];
-            System.arraycopy(cards, 0, newCards, 0, size);
-            cards = newCards;
-        }*/
-        private void expandArray(int additionalCapacity) {
-            int newCapacity = cards.length + additionalCapacity;
-            Card[] newCards = new Card[newCapacity];
-            System.arraycopy(cards, 0, newCards, 0, size);
-            cards = newCards;
         }
     }
 
+    // Procedure add(): add new cards
+    public void add(Card... newCards) {
+        int newSize = size + newCards.length; //check if new cards fit into current array
+        if (newSize > cards.length) {
+            expandArray(newCards.length); //if it doesn't fit > method to expand array
+        }
+        for (int i = 0; i < newCards.length; i++) { //load new cards into array
+            Card card = newCards[i];
+            if (card != null) {
+                cards[size++] = card;
+            }
+        }
+    }
+
+    // Procedure add(): add cards of another hand
+    public void add(Hand otherHand) {
+        Card[] otherHandCards = otherHand.getHandCards();
+        for (int i = 0; i < otherHandCards.length; i++) {
+            Card card = otherHandCards[i];
+            if (card != null) {
+                if (size == cards.length) {
+                    expandArray(cards.length);
+                }
+                cards[size++] = card;
+            }
+        }
+    }
+
+    // Function isSuited(): check if cards have the same suit
+    public boolean isSuited() {
+        if (size == 0) return true; //empty hand > suited
+        Suit suit = cards[0].getSuit(); //color of first card as reference
+        for (int i = 1; i < size; i++) { //compare all cards to first card
+            if (!cards[i].getSuit().equals(suit)) { 
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Function getHandCards(): returns all cards in hand
+    public Card[] getHandCards() {
+        Card[] currentCards = new Card[size];
+        for (int i = 0; i < size; i++) { //cards get loaded into currentcards
+            currentCards[i] = cards[i];
+        }
+        return currentCards;
+    }
+
+    // Procedure setHandCards(): sets hand on new cards
+    public void setHandCards(Card... newCards) {
+        this.cards = new Card[newCards.length];
+        this.size = 0;
+        for (int i = 0; i < newCards.length; i++) {
+            Card card = newCards[i];
+            if (card != null) {
+                this.cards[size++] = card; //++ du prepare the array to save new card
+            }
+        }
+    }
+
+    // expand array
+    private void expandArray(int additionalCapacity) {
+        int newCapacity = cards.length + additionalCapacity;
+        Card[] newCards = new Card[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newCards[i] = cards[i];
+        }
+        cards = newCards;
+    }
+}
