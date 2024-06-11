646348ab6c90bc0237d9e48c3111c246b0dd0ae7
Kiwi <167688339+yoonniu@users.noreply.github.com>
2024-06-11 17:17:02 +0200

    Update SavingsAccount.java

--- a4x2/src/bankAccount/SavingsAccount.java
+++ a4x2/src/bankAccount/SavingsAccount.java
@@ -7,6 +7,8 @@ public class SavingsAccount extends BankAccount {
     //Parameter werden gegeben und initialisiert
     public SavingsAccount (String iban, long startguthaben, int zinssatz) {
         super(iban, startguthaben);
+        assert iban != null: "ungültige IBAN.";
+        assert startguthaben >= 0: "Startguthaben kann nicht negativ sein.";
         //TODO DONE assert für zinssatz
         this.zinssatz = zinssatz;
         assert zinssatz >= 0: "Zinssatz ist nicht möglich.";
@@ -29,7 +31,7 @@ public class SavingsAccount extends BankAccount {
     public String toString() {
         //TODO hier spezielle Attribute und toString von BankAccount ausgeben / nutzen
         //TODO auch im CrrentAccount
-        return String.format("<%s>: Konto:%s, Kontostand:%d, Zinssatz:%d", SavingsAccount.class.getSimpleName(), getAccountId(), getAccountBalance(), zinssatz);
+        return String.format("<%s>: %s, Zinssatz:%d", SavingsAccount.class.getSimpleName(), super.toString(), zinssatz);
     }
 
 }
