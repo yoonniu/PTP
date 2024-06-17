9c9e9d0ff432d1c0d69e315da0c27e159c9c10f3
Kiwi <167688339+yoonniu@users.noreply.github.com>
2024-06-17 23:15:56 +0200

    Update UsualOrder.java

--- a5x3/src/cardComparator/UsualOrder.java
+++ a5x3/src/cardComparator/UsualOrder.java
@@ -16,24 +16,24 @@ import java.util.*;
 
 public class UsualOrder implements Comparator<Card> {
     
+    //desired order
     Rank[] rankOrder = new Rank[] {ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO};
     Suit[] suitOrder = new Suit[] {CLUB, SPADES, HEART, DIAMOND};
 
     @Override
     public int compare(Card c1, Card c2) {
         
+        //Position im Enum wird ermittelt und in rankOrder abgeglichen
+        //Positionen werden subtrahiert > entscheidet wie sortiert wird
         int compareRank = rankOrder[c1.getRank().ordinal()].ordinal() -
                 rankOrder[c2.getRank().ordinal()].ordinal();
         
         int compareSuit = suitOrder[c1.getSuit().ordinal()].ordinal() -
               suitOrder[c2.getSuit().ordinal()].ordinal();
-
+        
+        //ternäre Operator > if else
+        //Bedingung ? Ausdruck1 : Ausdruck2 
+        //wenn die Ränge unterschiedlich sind wird nach ihnen sortiert, wenn nicht nach Farben (suit)
         return compareRank != 0 ? compareRank : compareSuit;
     }
 }
-
-
-
-
-
-
