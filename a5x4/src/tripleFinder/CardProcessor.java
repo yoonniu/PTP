a1b53391f6597d0c8b4e6fdc984dc9df399be9c0
Kiwi <167688339+yoonniu@users.noreply.github.com>
2024-06-20 15:06:53 +0200

    Update 5x4 final

--- a5x4/src/tripleFinder/CardProcessor.java
+++ a5x4/src/tripleFinder/CardProcessor.java
@@ -9,7 +9,7 @@ public class CardProcessor implements CardProcessor_I {
 
     //create map to save cards
     //Name of the card is the key and saved as Rank and amount of cards is saved as Arraylist
-    private Map<Rank,Collection<Card>> cardsMap = new HashMap<Rank,Collection<Card>>();
+    private final Map<Rank,Collection<Card>> cardsMap; /// = new HashMap<Rank,Collection<Card>>();
 
     //constructor without parameters
     public CardProcessor() {
@@ -20,27 +20,33 @@ public class CardProcessor implements CardProcessor_I {
     public Object process(Card card) {
         assert card != null : "Card is not valid"; //check if card is valid 
         
-        Rank currentRank = card.getRank();
+        final Rank currentRank = card.getRank();
         //get collection (arraylist) of current rank 
         Collection<Card> rankCollection = cardsMap.get(currentRank); 
         
-        //if Rank of currentCard is in HashMap 
+        if (rankCollection==null) {
+            rankCollection = new ArrayList<>();
+            cardsMap.put(currentRank, rankCollection);
+        }
+        rankCollection.add(card);
+        /*
+        //if Rank(key) of currentCard exists in hashmap 
         if (cardsMap.containsKey(currentRank)) {
-            
             rankCollection.add(card); //add card to collection
-            
-        }else{
+        }else{ 
             //add key to map and create new arraylist to store cards of rank
             rankCollection = new ArrayList<>(); //create new arraylist 
             rankCollection.add(card);
             cardsMap.put(currentRank, rankCollection); //put key and value in map
         }
-
+        */
         //if arraylist has 3 cards -> return the three cards
-        if (rankCollection.size() >= 3) return rankCollection;
+        if (rankCollection.size() >= 3) {
+            return cardsMap.remove(currentRank);
+        }
         
-    return null;
-}
+        return null;
+    }
 
     //Hashmap leeren
     @Override
