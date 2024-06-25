e72f3fa63f7d0e97f925e6d6eb863975d26402f6
Kiwi <167688339+yoonniu@users.noreply.github.com>
2024-06-25 16:03:46 +0200

    Add files via upload

---
+++ z2x2/src/sieveOfEratosthenes/PrimeFinder.java
@@ -0,0 +1,63 @@
+// This source code is UTF-8 coded - see https://stackoverflow.com/questions/9180981/how-to-support-utf-8-encoding-in-eclipse
+package sieveOfEratosthenes;
+
+
+/**
+ * prime finder using sieve of Eratosthenes algorithm<br />
+ * Eine Erklärung des "Sieb des Eratosthenes" findest sich z.B. auf Wikipedia
+ * -> https://de.wikipedia.org/wiki/Sieb_des_Eratosthenes
+ * 
+ * @author   (your name(s)) 
+ * @version  (a version number or a date)
+ */
+public class PrimeFinder {
+
+    /**
+     * find primes using sieve of Eratosthenes
+     * 
+     * @param sieveEnd  the last number of the sieve resp. last number to be checked if prime
+     */
+    public void printPrimes( int sieveEnd ){
+        //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        //###
+        //###
+        //###
+        //###           HIER kommt Ihr Code hin
+        //###
+        //###                    VVVV
+        //###                    VVVV
+        //###                    VVVV
+        //###   VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+        //###      VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+        //###         VVVVVVVVVVVVVVVVVVVVVVVVVV
+        //###            VVVVVVVVVVVVVVVVVVVV
+        //###               VVVVVVVVVVVVVV
+        //###                  VVVVVVVV
+        //###                     VV
+
+
+        // Fügen Sie hier Ihren Code ein
+        // bzw. ersetzen Sie diesen Kommentar durch Ihren Code.
+        // ...
+        int sieve[] = new int[sieveEnd]; //Deklaration und Initialisierung
+
+        for (int i = 0; i < sieveEnd; i++) {
+            sieve[i] = i+1;
+        }
+
+        sieve[0] = 0;
+        for (int i = 0; i < sieveEnd; i++) {
+            for (int j = 0; j < sieveEnd; j++) {
+                if (sieve[j] != 0) {
+                    if (sieve[i] % 2 == 0) {
+                        sieve [i] = 0;
+                    }
+                }
+            }
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        System.out.flush();
+    }//method()
+
+}//class
