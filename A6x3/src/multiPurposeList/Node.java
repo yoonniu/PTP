4cd59203b05a9b151c9efa9784d16ac9c52ef448
Kiwi <167688339+yoonniu@users.noreply.github.com>
2024-07-02 19:02:33 +0200

    Add files via upload

---
+++ A6x3/src/multiPurposeList/Node.java
@@ -0,0 +1,29 @@
+package multiPurposeList;
+
+public class Node<T> {
+    
+    T info;
+    Node<T> prev;
+    Node<T> next;
+    int position;
+    
+    //Constructor
+    public Node(T info){
+        this.info = info;
+        this.next = null;
+        this.prev = null;
+        this.position = position;
+        }
+    
+//  T info;
+//  Node next;
+//  int position;
+//  
+//  //constructor
+//  public Node (T info) {
+//      this.info = info;
+//      this.next = null;
+//      this.position = position;
+//  }
+
+}
